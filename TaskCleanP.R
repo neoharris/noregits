0e3216ade0805c3ae4e8e4e87e4b027501d99fac
emmahelman <emmaromany@gmail.com>
2025-12-12 14:15:18 +0000

    merged data

--- TaskCleanP.R
+++ TaskCleanP.R
@@ -1,8 +1,32 @@
 Pclean <- read.csv("P.csv")
-View(Pclean)
+Aclean = read.csv("A.csv")
+Hclean <- read.csv("TaskData.csv")
 
+Hclean <- Hclean[,c("Age", "Gender", "Smoker", "PH", "MH", "Belief", "SES5")]
+
+Hclean$Region <- c("H", "H", "H", "H", "H", "H", "H", "H", "H", "H", "H", "H","H", "H", "H", "H", "H", "H")
+
+Hclean$SES5 <- c("", "", "", "", "", "", "", "", "", "", "", "","", "", "", "", "", "")
+Hclean$PH <- Hclean$Physical
+Hclean$MH <-Hclean$Mental
+View(Hclean)
+
+Bclean <-read.csv("B.csv")
+
+View(Pclean)
+View(Aclean)
+View(Bclean)
+View(Hclean)
+
+
+#Change all No values to N, Yes values to Y 
 Pclean$PH[Pclean$PH %in% c("No")]="N"
 Pclean$PH[Pclean$PH %in% c("Yes")]="Y"
+
+#Combining data 
+
+total <- rbind(Hclean, Pclean, Aclean)
+View(total)
 
 
 
