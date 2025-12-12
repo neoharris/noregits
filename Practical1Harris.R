b5a9ddcda5a823456dc618e101894c9835339637
Harris Nageswaran <hn366@cam.ac.uk>
2025-12-12 13:35:37 +0000

    m

--- Practical1Harris.R
+++ Practical1Harris.R
@@ -2,3 +2,11 @@
 PracA <- read.csv("Q1_data.csv")
 
 str(PracA)
+
+table(PracA$Sex)
+
+PracA <- subset(PracA, Sex != "")
+table(PracA$Sex)
+
+PracA$Sex [PracA$Sex %in% c("f", "female")]="F" 
+table(PracA$Sex)
